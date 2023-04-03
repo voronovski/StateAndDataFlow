e629d10578b7d4c8e36d73b1bfaddbc2586bf3bd
voronovski <avoronovski@gmail.com>
2023-04-02 21:43:35 -0700

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Aleksei Voronovskii on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
